83b152ecd4b2ae5bad06febd8ddd05ee10024682
katalysatorn <blair.burton@outlook.com>
2018-02-27 18:55:25 +0100

    Changed where styles are fetched from

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,18 @@
 const path = require('path')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
-
-const env = process.env.NODE_ENV || 'development'
-const isDev = env === 'developement'
-
-const extractCss = new ExtractTextPlugin({
-  filename: 'index.css',
-  disable: isDev
-})
 
 module.exports = {
   entry: {
-    bundle: './src/j/index.js'
+    bundle: './src/index.js'
   },
   output: {
     path: path.resolve(__dirname, 'docs'),
     filename: 'bundle.js'
   },
-  plugins: [
-    extractCss
-  ],
   module: {
     rules: [{
       test: /\.js$/,
       exclude: /node_modules/,
       use: 'babel-loader'
-    }, {
-      test: /\.css$/,
-      exclude: /node_modules/,
-      use: [
-        {loader: 'style-loader'},
-        {loader: 'css-loader'}
-      ]
     }]
   }
 }
